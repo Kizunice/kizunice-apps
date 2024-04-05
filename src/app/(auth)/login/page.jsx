4ca5edd1af4cabdd654e20a1a989150890ec3fc9
Kizunice <dev.kizunice@gmail.com>
2024-04-05 18:18:05 +0700

    fixed login theme

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 export default function Login() {
     return (
-      <section className="bg-primary">
+      <section className="bg-white">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full rounded-lg shadow-2xl md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="w-full bg-secondary rounded-lg shadow-2xl md:mt-0 sm:max-w-md xl:p-0 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <div className="flex mb-6 justify-center items-center w-30 h-auto">
                   <Link href={"/"} className='mx-auto'>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 export default function Register() {
   return (
-    <section className="bg-primary">
+    <section className="bg-white">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full rounded-lg shadow-2xl md:mt-0 sm:max-w-md xl:p-0">
+        <div className="w-full bg-secondary rounded-lg shadow-2xl md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <div className="flex mb-6 justify-center items-center w-30 h-auto">
                 <Link href={"/"} className='mx-auto'>
