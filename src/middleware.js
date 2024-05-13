a2c9ceb1e64e62b7320fba64437e26ffb95516fa
Kizunice <dev.kizunice@gmail.com>
2024-05-14 05:29:52 +0700

    fixed cookies

--- src/middleware.js
+++ src/middleware.js
@@ -2,17 +2,24 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(request) {
-  const session = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  console.log('session: ', session);
+  // const session = await getToken({
+  //   req: request,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
 
-  if (!session) return NextResponse.redirect(new URL('/login', request.url));
+  // if (session) return NextResponse.redirect(new URL('/', request.url));
 
-  if (session) return NextResponse.redirect(new URL('/', request.url));
+  // if (!session) return NextResponse.redirect(new URL('/login', request.url));
 
-  return NextResponse.next();
+  // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
+  // Getting cookies from the request using the `RequestCookies` API
+  const currentUser = request.cookies.get('next-auth.session-token')?.value;
+  console.log(currentUser); // => { name: 'nextjs', value: 'fast', Path: '/' }
+
+  if (!currentUser) {
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    return response;
+  }
 }
 
 export const config = {
