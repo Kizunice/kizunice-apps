ecc96f2767628f2145b99fd95ac1d74f36449de7
Kizunice <dev.kizunice@gmail.com>
2024-05-28 06:39:08 +0700

    still fixing middleware 3rd

--- src/middleware.js
+++ src/middleware.js
@@ -14,8 +14,8 @@ export async function middleware(request) {
 
   if (!session && protectedRoutes.includes(path)) {
     return NextResponse.redirect(new URL("/login", request.url));
-  } else if (session && path === '/') {
-    return NextResponse.redirect(new URL("/", request.url));
+  } else if (session && path === '/dashboard') {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
   
   return NextResponse.next();
