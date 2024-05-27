931bde410bbb361864f742917c7e3e60209de8aa
Kizunice <dev.kizunice@gmail.com>
2024-05-27 23:57:21 +0700

    fixed 7th middleware

--- src/middleware.js
+++ src/middleware.js
@@ -9,23 +9,31 @@ export async function middleware(request) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
+  const protectedRoutes = ["/dashboard", "/attendance", "/learning","/jobs","/document","/finance","/data-student","/data-partner","/data-sensei", ]
 
-  if (!session && 
-    path === '/dashboard' || 
-    path === '/attendance' || 
-    path === '/learning' || 
-    path === '/jobs' || 
-    path === '/data-student' || 
-    path === '/data-sensei'|| 
-    path === '/data-partner' || 
-    path === '/document' || 
-    path === '/finance'
-  ) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  } else if (session && path === '/login') {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+
+  if (!session && protectedRoutes.includes(path)) {
+    const absoluteURL = new URL("/login", request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  } 
   return NextResponse.next();
+
+  // if (!session && 
+  //   path === '/dashboard' || 
+  //   path === '/attendance' || 
+  //   path === '/learning' || 
+  //   path === '/jobs' || 
+  //   path === '/data-student' || 
+  //   path === '/data-sensei'|| 
+  //   path === '/data-partner' || 
+  //   path === '/document' || 
+  //   path === '/finance'
+  // ) {
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // } else if (session && path === '/login') {
+  //   return NextResponse.redirect(new URL('/dashboard', request.url));
+  // }
+  // return NextResponse.next();
 }
 
 export const config = {
