49eb44f0ae5308d9fbd555c4f9742e495c708112
Kizunice <dev.kizunice@gmail.com>
2024-08-13 02:45:34 +0700

    feature doc cv done rev 8

--- src/app/api/document/route.js
+++ src/app/api/document/route.js
@@ -27,7 +27,7 @@ export async function POST(req,res) {
         },
     });
 
-    const path = "/public/doc/CV-Siswa.xlsx"
+    const path = "public/doc/CV-Siswa.xlsx"
     const wb = new ExcelJS.Workbook()
     wb.xlsx.readFile(path)
         .then(async function() {
