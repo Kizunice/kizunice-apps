50add1239c1a02070efabbcc764f8d5a6bc582d2
Kizunice <dev.kizunice@gmail.com>
2024-09-06 05:49:41 +0700

    rev middleware 5

--- src/middleware.js
+++ src/middleware.js
@@ -19,12 +19,10 @@ export async function middleware(req) {
   if(!session && protectedRoutes.includes(path)) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
-  
-  return NextResponse.next(); 
 }
 
-export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|images|favicon.ico).*)'
-  ],
-}
+// export const config = {
+//   matcher: [
+//     '/((?!api|_next/static|_next/image|images|favicon.ico).*)'
+//   ],
+// }
